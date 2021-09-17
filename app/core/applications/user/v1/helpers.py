0f63b47cdcabdcadc6ad298c6dedea8548460d37
Jose <jose.ricardo@zinobe.com>
2021-09-16 21:59:29 -0500

    se genera estructura de las aplicaciones

---
+++ app/core/applications/user/v1/helpers.py
@@ -0,0 +1,47 @@
+import json
+import math
+
+
+def round_well(num):
+    return math.ceil(num)
+
+
+def meta_paginate(data_filter, params, query_set):
+    item_per_page = int(params.get("item_per_page", 15))
+    page = int(params.get("page", 1))
+    order = params.get("order", "DESC")
+    skip = (page-1) * item_per_page
+    key_orders = {
+        "ASC": "+",
+        "DESC": "-"
+    }
+    str_order = "{}created_at".format(key_orders.get(order))
+
+    total = query_set.all(data_filter).count()
+
+    total_page = round_well(total/item_per_page)
+
+    query_result = query_set.filter_all(
+        data_filter,
+        skip,
+        item_per_page,
+        str_order,
+    )
+
+    meta = {
+        "current_page": page,
+        "item_per_page": item_per_page,
+        "total_page": total_page,
+        "available_orders": [
+            "ASC",
+            "DESC"
+        ]
+    }
+
+    output = {
+        "data": json.loads(query_result.to_json())
+    }
+
+    output['meta'] = meta
+
+    return output
---
+++ app/core/applications/user/v1/serializers.py
@@ -0,0 +1,33 @@
+from enum import Enum
+from typing import List
+
+from pydantic import BaseModel
+
+
+class ProductSerializer(BaseModel):
+    name: str
+    description: str
+    stock: float
+
+
+class MetaPaginate(BaseModel):
+    current_page: int
+    item_per_page: int
+    total_page: int
+    available_orders: List
+
+
+class BasePaginate(BaseModel):
+    data: List
+    meta: MetaPaginate
+
+
+class User(BaseModel):
+    name: str
+    last_name: str
+    email: str
+    address: List
+
+
+class UserPaginate(BasePaginate):
+    data: List[User]
